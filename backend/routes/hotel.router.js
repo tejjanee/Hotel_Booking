7c415bf57c5c0e8a419d31ee4d114b8987f8067d
TEJA RAM <135523042+tejjanee@users.noreply.github.com>
2024-02-15 00:12:40 +0530

    Delete backend directory

--- backend/routes/hotel.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const getAllHotelHandler = require("../controllers/hotelController");
-
-router.route("/")
-    .get(getAllHotelHandler)
-
-module.exports = router;
--- backend/routes/singlehotel.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const singlehotelHandler = require("../controllers/singleHoterController");
-
-router.route("/:id")
-    .get(singlehotelHandler)
-
-module.exports = router;
