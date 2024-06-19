9d74117dae9789ad7698dd7b2e19262659d05bfe
TEJA RAM <135523042+tejjanee@users.noreply.github.com>
2024-06-19 16:51:55 +0530

    Delete backend/routes directory

--- backend/routes/category.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const categoryHandler = require("../controllers/categoryController");
-
-router.route("/")
-    .get(categoryHandler)
-
-module.exports =  router;
